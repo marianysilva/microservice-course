af3c378fad009fc93403a378da7562050e86049c
Mariany Ferreira da Silva <fsilvamariany@gmail.com>
2024-06-04 21:35:15 -0300

    feat(subject) fix: apply review suggestions

--- internal/matrix/endpoints/delete_matrix_subject.go
+++ internal/matrix/endpoints/delete_matrix_subject.go
@@ -15,8 +15,8 @@ import (
 )
 
 type DeleteMatrixSubjectRequest struct {
-	MatrixUUID  uuid.UUID `json:"matrix_uuid"  validate:"required"`
-	SubjectUUID uuid.UUID `json:"subject_uuid" validate:"required"`
+	MatrixUUID  uuid.UUID `json:"matrix_uuid"  validate:"required,uuid"`
+	SubjectUUID uuid.UUID `json:"subject_uuid" validate:"required,uuid"`
 }
 
 type DeletedMatrixSubjectResponse struct {
@@ -84,12 +84,12 @@ func decodeDeleteMatrixSubjectRequest(_ context.Context, r *http.Request) (inter
 
 	MatrixUUID, ok := vars["matrix_uuid"]
 	if !ok {
-		return nil, fmt.Errorf("invalid argument")
+		return nil, fmt.Errorf("invalid argument: missing matrix_uuid")
 	}
 
 	SubjectUUID, ok := vars["subject_uuid"]
 	if !ok {
-		return nil, fmt.Errorf("invalid argument")
+		return nil, fmt.Errorf("invalid argument: missing subject_uuid")
 	}
 
 	var req DeleteMatrixSubjectRequest
